1b3d6650035807b3f91ff1a68b0d43a90e738719
Ganesh Rajkumar Shete <136620219+Code-Crafter-Ganesh@users.noreply.github.com>
2023-11-10 23:09:48 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Predict the Column/predict-the-column.java
@@ -0,0 +1,74 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        
+        while(t-- > 0)
+        {
+            int N = Integer.parseInt(read.readLine());
+            int arr[][] = new int[N][N];
+            
+            for(int i = 0; i < N; i++)
+            {
+                int k = 0;
+                String str[] = read.readLine().trim().split("\\s+");
+                for(int j = 0; j < N; j++){
+                  arr[i][j] = Integer.parseInt(str[k]);
+                  k++;
+                }
+            }
+            
+            System.out.println(new Solution().columnWithMaxZeros(arr,N));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    int columnWithMaxZeros(int arr[][], int N)
+    {
+        // code here
+         int maxCount=0;
+        int currentCount=0;
+        int column=0;
+        int i,j;
+        
+        for(i=0;i<N;i++)
+        {
+            currentCount=0;
+            for(j=0;j<N;j++)
+            {
+                if(arr[j][i]==0)
+                {
+                    currentCount++;
+                }
+            }
+            
+            if(currentCount>maxCount)
+            {
+                maxCount=currentCount;
+                column=i;
+            }
+        }
+        
+        if(maxCount==0)
+        {
+            return -1;
+        }else{
+            return column;
+        }
+    }
+}
